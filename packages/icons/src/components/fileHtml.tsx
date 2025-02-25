3ab631a02138f45ca3b9820d27670e52189a285b
dev <moises.macedo@hotmail.com.br>
2025-02-25 11:49:19 -0300

    feat: added icons files

---
+++ packages/icons/src/components/fileHtml.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+type GenericIconProps = React.ComponentPropsWithoutRef<"svg">;
+
+export const FileHtml = ({ ...props }: GenericIconProps) => (
+    <svg
+        width={16}
+        height={17}
+        viewBox="0 0 16 17"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+        {...props}
+    >
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M4.38 11.565a.5.5 0 01.5-.5h2.08a.5.5 0 010 1H4.88a.5.5 0 01-.5-.5z"
+            fill="currentColor"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M5.92 11.065a.5.5 0 01.5.5v2.6a.5.5 0 11-1 0v-2.6a.5.5 0 01.5-.5zM1.5 11.065a.5.5 0 01.5.5v2.6a.5.5 0 11-1 0v-2.6a.5.5 0 01.5-.5zM3.32 11.065a.5.5 0 01.5.5v2.6a.5.5 0 11-1 0v-2.6a.5.5 0 01.5-.5z"
+            fill="currentColor"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M1 12.866a.5.5 0 01.5-.5h1.82a.5.5 0 010 1H1.5a.5.5 0 01-.5-.5zM12.94 11.065a.5.5 0 01.5.5v2.1h1.06a.5.5 0 110 1h-1.56a.5.5 0 01-.5-.5v-2.6a.5.5 0 01.5-.5zM8.367 11.09a.5.5 0 01.56.185l.893 1.25.893-1.25a.5.5 0 01.907.29v2.6a.5.5 0 11-1 0v-1.04l-.394.551a.5.5 0 01-.813 0l-.393-.55v1.04a.5.5 0 01-1 0v-2.6a.5.5 0 01.347-.477zM2.8 2.706a.02.02 0 00-.02.02v6.24a.5.5 0 11-1 0v-6.24a1.02 1.02 0 011.02-1.02h6.24a.5.5 0 01.354.146l3.64 3.64a.5.5 0 01.146.354v3.12a.5.5 0 01-1 0V6.053L8.833 2.706H2.8z"
+            fill="currentColor"
+        />
+        <path
+            fillRule="evenodd"
+            clipRule="evenodd"
+            d="M9.04 1.706a.5.5 0 01.5.5v3.14h3.14a.5.5 0 110 1H9.04a.5.5 0 01-.5-.5v-3.64a.5.5 0 01.5-.5z"
+            fill="currentColor"
+        />
+    </svg>
+);
