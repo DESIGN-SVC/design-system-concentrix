4e507942f6f218c6b54e076de36414778325a64e
dev <moises.macedo@hotmail.com.br>
2025-03-25 11:33:17 -0300

    chore: added types

--- src/stories/Icons/Icons.tsx
+++ src/stories/Icons/Icons.tsx
@@ -46,7 +46,7 @@ export const Icons = () => {
                     return (
                         name.toLowerCase().includes(searchLower) ||
                         tags.some((tag) =>
-                            tag.toLowerCase().includes(searchLower)
+                            (tag as string).toLowerCase().includes(searchLower)
                         )
                     );
                 }),
@@ -61,7 +61,7 @@ export const Icons = () => {
                     "flex flex-col gap-10 =",
                     "px-3 pb-5 w-full max-w-7xl shadow-md mx-auto rounded bg-white",
                     "sticky top-0 z-10",
-                    'transition-all ease-in-out duration-300'
+                    "transition-all ease-in-out duration-300",
                 ])}
             >
                 <h5 className="text-2xl font-semibold text-center">
@@ -107,10 +107,7 @@ export const Icons = () => {
                         ])}
                         placeholder="Search"
                     />
-                    <datalist
-                        id="titleIcon"
-                        className="appearance-none"
-                    >
+                    <datalist id="titleIcon" className="appearance-none">
                         {listTitleIcon.map((el) => (
                             <option key={el}>{el}</option>
                         ))}
